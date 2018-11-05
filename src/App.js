0d1e9247191af2413683e6b22687078e2bc6ca8f
github username <purnima.nyc@gmail.com>
2018-11-05 16:54:22 -0500

    update - header information

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Company Directory</h1>
+        <h1>Company Name and Logo - Placeholder</h1>
 
         <div className="navbar">
           <p>NavBar</p>
