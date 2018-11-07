8be55f2192f235a28e5533da9a4eb594baf24ed2
github username <purnima.nyc@gmail.com>
2018-11-07 14:31:57 -0500

    add - reducer function to manage state

--- src/store.js
+++ src/store.js
@@ -1,22 +1,35 @@
 import {
   createStore,
   applyMiddleware,
-  combinedReducers
-} from 'redux'
+  combineReducers
+} from 'redux';
 
 import thunk from 'redux-thunk';
 
 const employeesReducer = (state=[], action) => {
   switch(action,type) {
     case 'GET_EMPLOYEES_SUCCESS':
-      return action.employee;
+      return action.employees;
 
 
-    defaults
+    defaults:
       return state;
   }
 
 }
 
-const employees = employeesReducer();
-console.log(employee)
+const reducers = combineReducers({
+  employees: employeesReducer
+});
+
+const middleware = [thunk];
+
+export default createStore(
+  reducers,
+  applyMiddleware(...middleware),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+ );
+
+
+
+
