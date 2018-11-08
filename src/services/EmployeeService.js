596f7faa0229d1c73550d8fe16432c9c23921a4d
github username <purnima.nyc@gmail.com>
2018-11-07 19:44:49 -0500

    clean up code

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,8 +1,10 @@
 const API_URL = process.env.REACT_APP_API_URL;
+const employeeApiUrl = "https://api.themoviedb.org/3"
 const EmployeeService = {
+  
   fetchEmployees() {
     
-    return fetch('http://localhost:3001/api/employees')
+    return fetch(`$(API_URL)/employees`)
     .then(res => res.json())   
     
   },
@@ -23,7 +25,18 @@ const EmployeeService = {
     return fetch('http://localhost:3001/api/employees', request)
     .then(res => res.json())  
     
-  }
+  },
+
+  // fetchSearchResults() {
+  //   const query = this.state.searchTerm
+    
+  //   return fetch(`${employeeApiUrl}/search/person?api_key=d294f330aac8c6ae0963b497558e9f36&query=${query}`)
+  //   .then(res => res.json())   
+    
+  // },
+
+
+
 }
 
 export default EmployeeService;
--- src/store.js
+++ src/store.js
@@ -1,34 +1,34 @@
-import {
-  createStore,
-  applyMiddleware,
-  combineReducers
-} from 'redux';
+// import {
+//   createStore,
+//   applyMiddleware,
+//   combineReducers
+// } from 'redux';
 
-import thunk from 'redux-thunk';
+// import thunk from 'redux-thunk';
 
-const employeesReducer = (state=[], action) => {
-  switch(action,type) {
-    case 'GET_EMPLOYEES_SUCCESS':
-      return action.employees;
+// // const employeesReducer = (state=[], action) => {
+// //   switch(action,type) {
+// //     case 'GET_EMPLOYEES_SUCCESS':
+// //       return action.employees;
 
 
-    defaults:
-      return state;
-  }
+// //     defaults:
+// //       return state;
+// //   }
 
-}
+// // }
 
-const reducers = combineReducers({
-  employees: employeesReducer
-});
+// const reducers = combineReducers({
+//   employees: employeesReducer
+// });
 
-const middleware = [thunk];
+// const middleware = [thunk];
 
-export default createStore(
-  reducers,
-  applyMiddleware(...middleware),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
- );
+// export default createStore(
+//   reducers,
+//   applyMiddleware(...middleware),
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//  );
 
 
 
