da651e82593e1fbcbeec33eb79e59ca346eed745
github username <purnima.nyc@gmail.com>
2018-11-10 20:32:53 -0500

    add - action types for sessions

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,47 +1,11 @@
-const API_URL = "http://localhost:3001/api"
-
-const loginSuccess = (employee) => {
-  return {
-    type: 'LOGIN',
-    employee: employee
-  }
-}
-
-const loginFailure = (errors) => {
-  return {
-    type: 'LOGIN_FAILURE',
-    errors: errors.message
-  }
-}
-
-export const login = (employee, history) => {
-  
-  return dispatch => {
-    return fetch(`${API_URL}/sessions`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-
-        body: JSON.stringify({employee})
-      })
-        .then(res => res.json())
-        .then((response) => {
-
-          if (response.errors) {
-
-            throw Error(response.errors);
-
-          } else{
-            sessionStorage.setItem('Token', response.token);
-            history.push("/")
-            dispatch(loginSuccess(response.employee))
-          }
-        })
-        .catch( errors => {
-
-          sessionStorage.clear()
-          dispatch(loginFailure(errors))
-        })
-  }
-}
+export const CLEAR_ERROR = 'CLEAR_ERROR';
+// export const DELETE_USER_CITY_SUCCESS = 'DELETE_USER_CITY_SUCCESS';
+// export const FETCH_FORECAST_SUCCESS = 'FETCH_FORECAST_SUCCESS';
+export const FIND_USER_SUCCESS = 'FIND_USER_SUCCESS';
+// export const FORECAST_ERROR = 'FORECAST_ERROR';
+// export const GET_ZIPCODE = 'GET_ZIPCODE';
+export const LOAD_USER_SUCCESS = 'LOAD_USER_SUCCESS';
+export const LOGOUT = 'LOGOUT';
+// export const SET_ZIPCODE = 'SET_ZIPCODE';
+export const UPDATE_USER_SUCCESS = 'UPDATE_USER_SUCCESS';
+export const USER_ERROR = 'USER_ERROR';
