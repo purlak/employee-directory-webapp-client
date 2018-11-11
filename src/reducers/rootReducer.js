ea378403de0ea4c5c02dc4d46bdef9a33d52a1bf
github username <purnima.nyc@gmail.com>
2018-11-10 20:36:08 -0500

    add - sessions and error reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,13 @@
 
 import { combineReducers } from 'redux';
-import employeesReducer from './employeesReducer';
-    
+// import employeesReducer from './employeesReducer';
+import session from './sessionReducer';
+import error from './errorReducer';    
      
 const rootReducer = combineReducers({
    //employees: employeesReducer
+   session,
+   error
   
 });
      
