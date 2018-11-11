cf4dfaa457d582bda2ac4c7811faf905a4b5a104
github username <purnima.nyc@gmail.com>
2018-11-10 20:33:52 -0500

    update - login container code to include mappropsto state and dispatch + update fetch request

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import { loginUser } from '../actions/sessionActions';
+import { connect } from 'react-redux';
+import { Redirect, withRouter } from 'react-router-dom';
+// import { bindActionCreators } from 'redux';
 
-const LoginApiUrl = "http://localhost:3001/api"
+// const LoginApiUrl = "http://localhost:3001/api"
 
 class Login extends Component {
+  // constructor (props) {
+    // super(props);
   state = {
+    employees: [],
     username: '',
-    password: ''
-  };
+    password: '',
+    // errors: ''
+  };  
+  
+  
 
   handleInput = event => {
     this.setState({[event.target.name]: event.target.value})
@@ -14,16 +24,71 @@ class Login extends Component {
 
   onLogin = (event) => {
     event.preventDefault();
-    const  query  = this.state.username
+    this.props.loginUser(this.state)
+    // this.setState({
+    //   email: "",
+    //   password: "",
+    //   errors: this.props.errors
+    // })
 
-    return fetch (`${LoginApiUrl}/sessions`)
-    .then(res => res.json())
+    // return fetch(`${LoginApiUrl}/sessions`, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
 
-    .then(data => this.setState({searchResults: data.results})) 
+    //     body: JSON.stringify(this.state)
+    //   })
+    //     .then(res => res.json())
+    //     .then((response) => {
+
+    //       // if (response.errors) {
+
+    //       //   throw Error(response.errors);
+
+    //       // } else{
+    //         sessionStorage.setItem('Token', response.token);
+            
+    //         //dispatch(loginSuccess(response.this.state))
+    //       // }
+    //     })
+
+    //this.props.login(this.state)
+    
+
+//     const  query  = this.state.username
+
+//     return fetch (`${LoginApiUrl}/sessions`)
+//     .then(res => res.json())
+//     .then((data) => {
+
+//           if (data.errors) {
+
+//             throw Error(data.errors);
+
+//           } else{
+//             sessionStorage.setItem('Token', data.token);
+//             history.push("/")
+//             //dispatch(loginSuccess(response.user))
+//           }
+//         })
+//         .catch( errors => {
+
+//           sessionStorage.clear()
+//           //dispatch(loginFailure(errors))
+// })
 
   }
 
   render() {
+     //console.log(this.state)
+    // const errors = this.state.errors
+    // if (this.props.employee.id) {
+    //   return (
+    //     <Redirect push to="/search" />
+    //   )
+    // } else {
+
     return (
       <div>
       <form>
@@ -50,15 +115,24 @@ class Login extends Component {
 
       <button onClick={this.onLogin} className="buttonSize">Login</button>
       </form>
-      <div>
-        
-      </div>
-    </div>
 
+      <div><span className="login-section__span">Not registered? </span><a className="login-section__links" href="/signup">Signup</a></div>
+      </div>
+        
+      
     )
 
   }
 
+// }
 }
 
-export default Login;
+const mapStateToProps = (state) => {
+  console.log (state.session.employee)
+  return {
+    employee: state.session.employee
+  }
+}
+
+
+export default connect(mapStateToProps, { loginUser })(Login);
