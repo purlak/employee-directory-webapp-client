22c1606a62d68523a5d415cb4faaf95edd424fcd
github username <purnima.nyc@gmail.com>
2018-11-12 08:51:36 -0500

    clean up code

--- src/components/employees.js
+++ src/components/employees.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Employees = ({ employees }) => {
 
   const renderEmployees = employees.map(employee =>
+    
     <div className="displayEmployee">
+
     <span key={employee.id} className="displayName">{employee.name}  <br/></span>
     </div>
   )
