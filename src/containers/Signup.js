482b80c6c2ff28412a28951eaddaac74f257ff58
github username <purnima.nyc@gmail.com>
2018-11-11 23:32:27 -0500

    add - signup form

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -20,6 +20,73 @@ render() {
   return (
     <div>
       <form>
+        <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Name"
+            value={this.state.name}
+            onChange={(event) => this.onInput(event)}
+            className="login"
+          />
+        <br />
+
+        <input
+            type="text"
+            id="username"
+            name="Username"
+            placeholder="Username"
+            value={this.state.username}
+            onChange={(event) => this.onInput(event)}
+            className="login"
+          />
+        <br />
+
+        <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={(event) => this.onInput(event)}
+            className="login"
+          />
+        <br />
+
+        <input
+            type="text"
+            id="title"
+            name="title"
+            placeholder="Title"
+            value={this.state.title}
+            onChange={(event) => this.onInput(event)}
+            className="login"
+          />
+        <br />
+
+        <input
+            type="text"
+            id="department"
+            name="department"
+            placeholder="Department"
+            value={this.state.department}
+            onChange={(event) => this.onInput(event)}
+            className="login"
+          />
+        <br />
+
+        <input
+            type="text"
+            id="location"
+            name="location"
+            placeholder="Location"
+            value={this.state.location}
+            onChange={(event) => this.onInput(event)}
+            className="login"
+          />
+        <br />
+
+        <button onClick={this.onLogin} className="buttonSize">SignUp</button>
 
       </form>
     </div>
