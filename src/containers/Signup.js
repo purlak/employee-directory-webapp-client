a6e250dce8a2998dbc8206795e80c9289ca036dc
github username <purnima.nyc@gmail.com>
2018-11-11 23:21:20 -0500

    update - set state, build out render

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -5,9 +5,28 @@ import { signupUser } from '../actions/sessionActions';
 
 class SignUp extends Component {
 
+state = {
+  name: '',
+  username: '',
+  password: '',
+  title: '',
+  department: '',
+  location: ''
+
+}
+render() {
 
 
+  return (
+    <div>
+      <form>
 
+      </form>
+    </div>
+
+  )
+
+}
 }
 
 export default SignUp;
